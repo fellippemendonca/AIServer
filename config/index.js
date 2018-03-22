a3a4312f4402561de49be05ba59f53410c8f994f
fellippemendonca <fellippe.mendonca@gmail.com>
2018-03-22 17:18:22 -0300

    SocketServer included

---
+++ config/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const serverAddress = {
+  host: 'localhost',
+  port: 3003
+}
+
+module.exports = serverAddress;
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 'use strict';
 
-let pino = require('pino');
-let logger = pino({
-  name: 'AIServer',
-  safe: true,
-  serializers: {
-    req: pino.stdSerializers.req,
-    res: pino.stdSerializers.res
-  }
-});
+const logger = require('./lib/logger/logger')();
 
-logger.info('hello world');
-logger.error('this is at error level');
-logger.info('the answer is %d', 42);
-logger.info({ obj: 42 }, 'hello world');
-logger.info({ obj: 42, b: 2 }, 'hello world');
-logger.info({ obj: { aa: 'bbb' } }, 'another');
-setImmediate(function () {
-  logger.info('after setImmediate')
-});
-logger.error(new Error('an error'));
+const socketServer = require('./lib/socketServer/socketServer');
+socketServer();
 
-var child = logger.child({ a: 'property' });
-child.info('hello child!');
+/*
+setInterval(() => {
 
-var childsChild = child.child({ another: 'property' });
-childsChild.info('hello baby..');
+  logger.info('hello world');
+  logger.error('this is at error level');
+  logger.info('the answer is %d', 42);
+  logger.info({ obj: 42 }, 'hello world');
+  logger.info({ obj: 42, b: 2 }, 'hello world');
+  logger.info({ obj: { aa: 'bbb' } }, 'another');
+  setImmediate(function () {
+    logger.info('after setImmediate')
+  });
+  logger.error(new Error('an error'));
+
+  var child = logger.child({ a: 'property' });
+  child.info('hello child!');
+
+  var childsChild = child.child({ another: 'property' });
+  childsChild.info('hello baby..');
+
+}, 1000);
+*/
+
